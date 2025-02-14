b63b9146557f59e738378692865ce20116c25e91
Alexander Chepurnoy <cany748@gmail.com>
2025-02-14 05:07:03 +0000

    chore: remove unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    {{ $t("main.quitStretchly") }}
+    {{ $t("") }}
+    {{ t("") }}
   </div>
 </template>
+
+<script setup lang="ts">
+import { useTranslation } from 'i18next-vue';
+
+const { t } = useTranslation();
+</script>
